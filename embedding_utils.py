072632600c20a2d2f4955f4a316e4e7ff9e212bb
nurselidemir <nurselidemr@icloud.com>
2025-06-25 23:14:46 +0300

    Add cosine similarity calculation for CV and job embeddings

--- embedding_utils.py
+++ embedding_utils.py
@@ -1,5 +1,8 @@
 from sentence_transformers import SentenceTransformer
 
+from numpy import dot
+from numpy.linalg import norm
+
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_cv_embedding(text: str):
@@ -10,12 +13,15 @@ def get_cv_embedding(text: str):
 # sentence-transformers içindeki SBERT modeline bu metni verir. model bu metni sayısal vektör haline getirir.
 # çıktıyı numpy array olarak verir.
 
-from numpy import dot
-from numpy.linalg import norm
+
 
 def calculate_similarity(embedding1, embedding2):
-     """
+    """
     İki embedding arasındaki cosine similarity oranını hesaplar.
     1.0 → tamamen aynı, 0.0 → alakasız
     """
     return dot(embedding1, embedding2) / (norm(embedding1) * norm(embedding2))
+
+# dot = iç çarpım fonksiyonudur.
+# norm = vektörün uzunluğunu (büyüklüğünü) bulur
+# linalg = linear algebra
