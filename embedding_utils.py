4c1b16ed70ecd97272c697fc3ade729f474dfc5c
nurselidemir <nurselidemr@icloud.com>
2025-06-25 23:05:37 +0300

    add cv embedding function using SBERT
    # Conflicts:
    #	embedding_utils.py

--- embedding_utils.py
+++ embedding_utils.py
@@ -1,1 +1,11 @@
 from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def get_cv_embedding(text: str):
+    """
+    CV içeriğini alır, embedding (vektör) olarak döner.
+    """
+    return model.encode(text, convert_to_numpy=True)
+# sentence-transformers içindeki SBERT modeline bu metni verir. model bu metni sayısal vektör haline getirir.
+# çıktıyı numpy array olarak verir.
